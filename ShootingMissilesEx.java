0ab28339b1c3c08b85b129da5ef12996db39baa6
BrendanMeisner <80916056+BrendanMeisner@users.noreply.github.com>
2021-10-04 11:32:14 -0500

    Add files via upload

---
+++ ShootingMissilesEx.java
@@ -0,0 +1,30 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class ShootingMissilesEx extends JFrame {
+
+    public ShootingMissilesEx() {
+        
+        initUI();
+    }
+    
+    public void initUI() {
+        
+        add(new Window());
+        
+        setSize(400, 300);
+        setResizable(false);
+        
+        setTitle("Shooting missiles");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            ShootingMissilesEx ex = new ShootingMissilesEx();
+            ex.setVisible(true);
+        });
+    }
+}
